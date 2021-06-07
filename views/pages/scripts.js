8ab46d9fa98ac2ec406c27e5c3bd4fb51f964f30
Agatha Almunir <agathaalmunir@gmail.com>
2021-06-07 17:59:31 -0400

    add logo, font, about content,, overlay images, and toggle

--- views/pages/scripts.js
+++ views/pages/scripts.js
@@ -16,6 +16,35 @@ hamburgerMenu.addEventListener("click", toggleMenu);
 let acc = document.getElementsByClassName("accordion");
 let i;
 
+// TOGGLE DISPLAY NONE/BLOCK ON FLOWER OPTIONS\
+
+// make and add classes to the labels
+// Select the label of the radio button. 
+const potOption = document.querySelector(".pot-option");
+// Select the option img using their class
+const potImg = document.querySelector(".pot");
+// Event handler funtion to toggle the selected class to the option imgs
+const togglePot = function() {
+  if (potImg.style.display === "none") {
+    potImg.style.display = "block"
+  } else {
+    potImg.style.display = "none"
+  }
+};
+// Add event listener to toggle when the label of the radio button is clicked
+potOption.addEventListener("click", togglePot);
+
+const ribbonOption = document.querySelector(".ribbon-option");
+const ribbonImg = document.querySelector(".ribbon");
+const toggleRibbon = function() {
+  if (ribbonImg.style.display === "none") {
+    ribbonImg.style.display = "block"
+  } else {
+    ribbonImg.style.display = "none"
+  }
+};
+ribbonOption.addEventListener("click", toggleRibbon);
+
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
